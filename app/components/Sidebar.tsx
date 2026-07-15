c2843f641dfb98b546b29bb973b0a453858b8e45
Diego Nunez <nunez.guzman@gmail.com>
2026-07-15 13:13:17 -0600

    feat: add user state

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -16,7 +16,7 @@ const menuItems = [
 
 export const Sidebar = () => {
   return (
-    <div id="menu" style={{ width: '400px'}} className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll">
+    <div id="menu" style={{ width: '400px'}} className="bg-gray-900 min-h-screen z-10 text-slate-300 left-0 h-screen overflow-y-scroll">
       <div id="logo" className="my-4 px-6">
         <h1 className="text-lg md:text-2xl font-bold text-white">Dash<span className="text-blue-500">8</span>.</h1>
         <p className="text-slate-500 text-sm">Manage your actions and activities</p>
--- app/dashboard/counter/page.tsx
+++ app/dashboard/counter/page.tsx
@@ -1,6 +1,28 @@
+"use client";
+
+import { useState } from "react";
+
 export const CounterPage = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleCounter = (operation: string) => {
+    setCounter(counter + (operation === 'sum' ? 1 : -1));
+  };
+
   return (
-    <div>CounterPage</div>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      CounterPage
+      <span>Productos en el carrito</span>
+      <span className="text-9xl">{counter}</span>
+      <div className="flex">
+        <button onClick={() => handleCounter('sum')} className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all mr-2">
+          +1
+        </button>
+        <button onClick={() => handleCounter('subtract')} className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all mr-2">
+          -1
+        </button>
+      </div>
+    </div>
   )
 }
 
