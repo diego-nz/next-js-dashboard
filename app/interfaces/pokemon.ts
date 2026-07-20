343fe118f16d6828d5ee3edbfe3c513da81141e7
Diego Nunez <nunez.guzman@gmail.com>
2026-07-20 12:54:25 -0600

    feat: add pokemon details

--- app/interfaces/pokemon.ts
+++ app/interfaces/pokemon.ts
@@ -14,3 +14,39 @@ export interface SinglePokemon {
   id: string;
   name: string;
 }
+//
+  interface Sprites {
+    front_default: string;
+    back_default: string;
+    front_shiny: string;
+    back_shiny: string;
+    other?: {
+      dream_world: {
+        front_default: string;
+      };
+    };
+  }
+
+  interface MoveEntry {
+    move: {
+      name: string;
+      url: string;
+    };
+  }
+
+  interface TypeEntry {
+    slot: number;
+    type: {
+      name: string;
+      url: string;
+    };
+  }
+
+  export interface Pokemon {
+    id: number;
+    name: string;
+    weight: number;
+    sprites: Sprites;
+    moves: MoveEntry[];
+    types: TypeEntry[];
+  }
