876fd7ec2fd73c626dcc694568920088ce470ab5
Diego Nunez <nunez.guzman@gmail.com>
2026-08-12 13:16:42 -0600

    feat: redux counter reducer and actions

--- app/shopping-cart/components/CartCounter.tsx
+++ app/shopping-cart/components/CartCounter.tsx
@@ -1,18 +1,20 @@
 "use client";
 
-import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/app/store";
+import { addOne, substractOne } from "@/app/store/counter/counterSlice";
 
 export const CartCounter = () => {
 
-const [counter, setCounter] = useState(0);
+const count = useAppSelector( state => state.counter.count);
+const dispatch = useAppDispatch();
 
 const handleCounter = (operation: string) => {
-  setCounter(counter + (operation === 'sum' ? 1 : -1));
+  return operation === 'sum' ? dispatch(addOne()) : dispatch(substractOne());
 };
 
   return (
     <>
-    <span className="text-9xl">{counter}</span>
+    <span className="text-9xl">{count}</span>
     <div className="flex">
         <button onClick={() => handleCounter('sum')} className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all mr-2">
           +1
