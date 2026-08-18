51087e999659441af34ea7f85ee4fc7fe6d3144b
Diego Nunez <nunez.guzman@gmail.com>
2026-08-18 15:02:26 -0600

    feat: empty favorite pokemons

--- app/components/pokemon/PokemonFavorites.tsx
+++ app/components/pokemon/PokemonFavorites.tsx
@@ -2,11 +2,26 @@
 
 import { useAppSelector } from "@/app/store";
 import { PokemonGrid } from "./PokemonGrid";
+// import { useState } from "react";
 
 const PokemonFavorites = () => {
   const favoritePokemons = useAppSelector( state => Object.values(state.pokemons));
+  // const [pokemons, setPokemons] = useState( favoritePokemons );
+
   return (
-    <PokemonGrid pokemonList={favoritePokemons} />
+    <>
+      {
+        favoritePokemons.length ? <PokemonGrid pokemonList={favoritePokemons} /> : <NoFavorites />
+      }
+    </>
+  )
+}
+
+export const NoFavorites = () => {
+  return (
+    <div className="flex flex-col h-[50vh] items-center justify-center">
+      <span className="text-red-500">No hay favoritos</span>
+    </div>
   )
 }
 
